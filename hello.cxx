1fcc7298b0586637f8023f932077270bfd35e833
cjd5653 <43938155+cjd5653@users.noreply.github.com>
2018-10-08 11:21:49 -0400

    Added hello.cxx
    The quintessential Hello World! program.

---
+++ hello.cxx
@@ -0,0 +1,13 @@
+// Connor Deardorff
+// 10/7/18
+// Description: The quintessential Hello World! program
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;	
+	
+	return 0;
+}
